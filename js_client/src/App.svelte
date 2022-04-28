e97e817b55ba2c27bd879a266ac9d1b67e8ea4df
Kieron Taylor <kt19@sanger.ac.uk>
2022-04-28 16:17:21 +0100

    Experiment with partitioning view

--- js_client/src/App.svelte
+++ js_client/src/App.svelte
@@ -9,12 +9,15 @@ function pipelineSelected(event) {
     pipeline_name = event.detail.pipeline_name
 }
 </script>
-
-<PipelineList on:message={pipelineSelected} tableStyle="stripey"/>
-
-{#if pipeline_name}
-<TaskList pipelineName={pipeline_name} tableStyle="stripey"/>
-{/if}
+<h1>Porch Pipeline Status Viewer</h1>
+<div class="upper">
+    <PipelineList on:message={pipelineSelected} tableStyle="stripey"/>
+</div>
+<div class="lower">
+    {#if pipeline_name}
+    <TaskList pipelineName={pipeline_name} tableStyle="stripey"/>
+    {/if}
+</div>
 
 <style>
     :global(table.stripey) {
@@ -24,11 +27,11 @@ function pipelineSelected(event) {
     :global(tr, th, td) {
         padding: 10px;
         text-align: left;
+        border: 1px solid;
     }
     :global(tr) {
         background-color: #ffffff;
         border: 5px;
-
     }
     :global(tr:nth-child(even)) {background-color: #a2a2a2;}
     :global(tr.selectable:hover) {background-color: #ddd;}
@@ -39,5 +42,17 @@ function pipelineSelected(event) {
     :global(caption) {
         font-size: large;
         font-weight: bold;
+    }
+    .upper {
+        position: absolute;
+        top: 10%;
+        bottom: 45%;
+        width: 100%;
+    }
+    .lower {
+        position: sticky;
+        top: 55%;
+        bottom: 95%;
+        width: 100%;
     }
 </style>
